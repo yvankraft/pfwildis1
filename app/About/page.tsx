c606f87980ee2dad73688259976f35c6f99f30d2
yvankraft <yvanngone53@gmail.com>
2026-03-06 15:59:17 +0100

    Refactor About page: streamline GSAP animations, enhance layout, and update content structure

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,15 +6,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { Canvas } from "@react-three/fiber";
-import {
-  useGLTF,
-  Environment,
-  Float,
-  Html,
-  useProgress,
-  Cloud,
-  Clouds,
-} from "@react-three/drei";
+import { useGLTF, Environment, Float } from "@react-three/drei";
 
 //initialisation du srollTrigger
 gsap.registerPlugin(ScrollTrigger);
@@ -44,23 +36,26 @@ function SingleModel({
     });
 
     tl.to(meshRef.current.position, {
-      z: 0.5,
+      y: 1,
       duration: 1,
-      ease: "power2.inOut",
+      ease: "expo",
     })
       .to(meshRef.current.position, {
-        z: 2.2,
+        z: 1.2,
         duration: 0.5,
+        ease: "expo",
       })
       .to(meshRef.current.rotation, {
-        y: Math.PI * 0.5, // 90 degrés seulement
+        y: -Math.PI * 0.6, // 90 degrés seulement
         duration: 1,
+        ease: "expo",
       })
       .to(meshRef.current.scale, {
-        x: 4,
-        y: 4,
-        z: 4,
+        x: 3,
+        y: 3,
+        z: 3,
         duration: 1.5,
+        ease: "expo",
       });
   }, []); // [] pour ne l'exécuter qu'au montage
 
@@ -78,6 +73,22 @@ function SingleModel({
 
 const page = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const INFO_DATA = {
+    electronics: {
+      title: "Électronique",
+      text: "Conception de systèmes embarqués et microcontrôleurs.",
+      media: "/media/pcb-render.jpg", // Chemin vers ton image
+      type: "image",
+      color: "border-blue-500",
+    },
+    drones: {
+      title: "Drones FPV",
+      text: "Intégration de propulsion brushless et télémétrie temps réel.",
+      media: "/media/drone-flight.mp4", // Chemin vers ta vidéo
+      type: "video",
+      color: "border-purple-500",
+    },
+  };
 
   useEffect(() => {
     // 1. Vérification initiale
@@ -91,15 +102,14 @@ const page = () => {
     return () => mediaQuery.removeEventListener("change", handler);
   }, []);
   return (
-    <div className="grid ">
+    <div className="grid dark:bg-zinc-900">
       <section className="fixed inset-0 h-screen w-full transition-all duration-1000 pointer-events-none bg-transparent dark:bg-from-zinc-900">
         <Canvas camera={{ position: [0, 2, 10], fov: 50 }}>
           {" "}
           {isDarkMode ? (
             <>
               {/* Mode Sombre : Ambiance tamisée avec lumière jaune */}
-              <ambientLight intensity={0.02} />
-              {/* Ajout des éléments de vie */}
+              <ambientLight intensity={1} />
             </>
           ) : (
             <>
@@ -109,21 +119,90 @@ const page = () => {
               <Environment preset="city" />
             </>
           )}
+          <SingleModel url="/turbine.glb" position={[0, -4, -5]} />
         </Canvas>
       </section>
-      <section className="h-screen flex flex-col justify-center items-center">
-        <h1 className=" text-7xl font-mono">Technic and Sofware</h1>
-        <p className="text-3xl font-mono">side by side</p>
-      </section>
-      <section>
+
+      <section className="relative pointer-events-auto">
+        <div className="h-screen flex flex-col justify-center items-center z-20 ">
+          <h1 className=" text-7xl font-bold uppercase tracking-tighter mix-blend-difference text-shadow-white">
+            Technic and Sofware
+          </h1>
+          <p className="text-3xl font-mono tracking-tighter text-shadow-white">
+            side by side
+          </p>
+        </div>
         <div
-          className=" lg:w-1/3  p-8 relative z-20 
+          className=" lg:w-4/7  p-8 relative z-20 
                 backdrop-blur-md bg-white/80 dark:bg-zinc-900/80 
                 border border-slate-400 dark:border-slate-700 
                 rounded-2xl shadow-2xl mr-[1%] ml-auto dark:shadow-slate-700"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sit
-          doloremque quo animi quam repellat rem id! Culpa labore suscipit
+          <h2 className="text-3xl font-bold uppercase mb-4 dark:text-white">
+            Sofware
+          </h2>
+          <p className="text-l font-bold uppercase mb-4 dark:text-white">
+            What makes me credible?
+          </p>
+          <p className="mb-4">
+            From the moment I could afford a laptop, I became interested in
+            programming. Although I already had a good foundation in C, I really
+            started spending nights programming with HTML and CSS thanks to
+            YouTube tutorials. Then I moved on to JavaScript.
+            <br />
+            Over time I became familiar with frameworks like React and for a
+            while I was very interested in React Native. But not for long.
+            <br />
+            Until recently I mainly used it quickly when I discovered three.js
+            then I switched to nextjs to familiarize myself with next and also
+            to have fewer problems with Vercel.
+            <br />
+            App development remains a field that greatly interests me, but for
+            the moment I prefer to refine my knowledge of Three.js.
+          </p>
+        </div>
+        <div className="lg:flex p-2 mt-50">
+          <div
+            className=" lg:w-4/7  p-8 relative z-20 
+                backdrop-blur-md bg-white/80 dark:bg-zinc-900/80 
+                border border-slate-400 dark:border-slate-700 
+                rounded-2xl shadow-2xl mr-[1%] ml-auto dark:shadow-slate-700"
+          ></div>
+          <div
+            className=" lg:w-4/7  p-8 relative z-20 
+                backdrop-blur-md bg-white/80 dark:bg-zinc-900/80 
+                border border-slate-400 dark:border-slate-700 
+                rounded-2xl shadow-2xl mr-[1%] ml-auto dark:shadow-slate-700"
+          >
+            <h2 className="text-3xl font-bold uppercase mb-4 dark:text-white">
+              technical and software
+            </h2>
+            <p className="text-l font-bold uppercase mb-4 dark:text-white">
+              What technical aspect are we going to talk about?
+            </p>
+            <p className="mb-4">
+              Before becoming a programmer, I first went through the field of
+              electronics, studying refrigeration and air conditioning in
+              secondary school, and subsequently my current studies in
+              electronics.
+              <br />
+              In my opinion, this aspect of my life is what pushes me, day by
+              day, to become interested in programming.
+              <br />
+            </p>
+            <p className="text-l font-bold uppercase mb-4 dark:text-white">
+              But in practice, what does that mean?
+            </p>
+            <p className="mb-4">
+              It's not the knowledge of a principle, whether it's programming or
+              electronic wiring, that motivates me, but rather the fascination
+              of learning how human intelligence pushes the boundaries and laws
+              of physics.
+            </p>
+            <p className="text-l font-bold uppercase mb-4 dark:text-white">
+              Here is some of my technical knowledge
+            </p>
+          </div>
         </div>
       </section>
     </div>
